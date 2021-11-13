4904b9fbc48f82153ec627eb97d9e3c11576a548
Daniel Yee <danielyee517@gmail.com>
2021-11-13 09:49:28 -0800

    Add JSON Web Token Auth

--- app/dtos.py
+++ app/dtos.py
@@ -1,6 +1,14 @@
 from typing import Optional
 from pydantic import BaseModel
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
+
 class UserDto(BaseModel):
     uid: Optional[str] = None
     username: Optional[str] = None
