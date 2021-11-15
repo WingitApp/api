773b92f649355e25e3f737e99294eae6de4fcfc4
Daniel Yee <danielyee517@gmail.com>
2021-11-15 01:31:05 -0800

    Connect to Neo4j using environment var NEO4J_BOLT_URL

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from neomodel import config 
+from neo4j import GraphDatabase
+from app.models import User
+import logging, os
+from neo4j.exceptions import ServiceUnavailable
 from app import app
 import uvicorn
 
 print("Starting database...")
-config.DATABASE_URL = 'bolt://neo4j:1234@127.0.0.1:7687'
+config.DATABASE_URL = os.environ["NEO4J_BOLT_URL"]
+print(config.DATABASE_URL)
 
 if __name__ == '__main__':
     uvicorn.run('main:app', reload=True)
